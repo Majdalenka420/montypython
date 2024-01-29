2ae43cac70e8cbcae071e1e8a45b9059d5fdd415
myke <MikulasZelinka@users.noreply.github.com>
2024-01-29 10:43:48 +0100

    zjednoduseni nadavek
    (:

--- nadavky.py
+++ nadavky.py
@@ -7,7 +7,7 @@ print("Generátor nadávek!")
 
 while True:
     input("Stiskni enter pro vygenerování")
-    print("")
-    print("Jsi " + cast_prvni[random.randint(0, len(cast_prvni)-1)] + " " + cast_druha[random.randint(0, len(cast_druha)-1)] + "!")
-    print("")
-    print("")
+    print()
+    print(f"Jsi {random.choice(cast_prvni)} {random.choice(cast_druha)}!")
+    print()
+    print()
